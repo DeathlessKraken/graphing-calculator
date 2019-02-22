63180bc7d106da9d120530c26b370ac5e75ba8a3
Sean Brown <michaelbrown.sean@gmail.com>
2019-02-21 18:06:53 -0800

    Proper toolbar and table label

--- graphcalc/__init__.py
+++ graphcalc/__init__.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-from graphcalc.widgets import Widgets
+from graphcalc.widgets import *
 
 WIDTH = 800
 HEIGHT = 600
@@ -9,15 +9,21 @@ HEIGHT = 600
 def create_app():
     # Configure main window environment
     root = Tk()
-    root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
+    #root.geometry(str(WIDTH) + 'x' + str(HEIGHT))
+    root.resizable(False, False)
     root.title("Graphing Calculator by Sean")
 
-    displayFrame = ttk.Frame(root, padding="3 3 12 12")
-    displayFrame.grid(column=0, row=0, sticky=(N, W, E, S))
-    root.columnconfigure(0, weight=1)
-    root.rowconfigure(0, weight=1)
+    mainFrame = ttk.Frame(root, padding="5")
+    mainFrame.grid(column=0, row=0, sticky='n s w e')
+
+    graphFrame = ttk.Frame(mainFrame, padding='5')
+    graphFrame.grid(row=0, column=0, sticky='n w')
+
+    #root.columnconfigure(0, weight=1)
+    #root.rowconfigure(0, weight=1)
 
     # Create new instance of widgets
-    w = Widgets(master=displayFrame)
+    w = Widgets(master=mainFrame)
+    g = Graph(master=graphFrame)
 
     return root
--- graphcalc/widgets.py
+++ graphcalc/widgets.py
@@ -1,6 +1,6 @@
 from tkinter import ttk
 from numpy import arange, sin, pi
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 
 
@@ -15,20 +15,33 @@ class Widgets(ttk.Frame):
         self.master.master.destroy()
 
     def create_widgets(self, master):
-        self.quit = ttk.Button(self, text="QUIT", command=self.exit)
-        self.quit.grid(row=0, column=0)
+        quit = ttk.Button(self, text="QUIT", command=self.exit)
+        quit.grid(row=0, column=0)
 
-        self.table_l = ttk.Label(self, text="Table").grid(row=1, column=0)
+        # This table label is being created in the mainFrame
+        table_l = ttk.Label(self.master, text="Table")
+        table_l.grid(row=0, column=1, sticky='n')
 
-        self.f = Figure(figsize=(7, 5), dpi=100)
-        self.a = self.f.add_subplot(111)
-        self.t = arange(0.0, 3.0, 0.01)
-        self.s = sin(2 * pi * self.t)
 
-        self.a.plot(self.t, self.s)
-        # self.a.set_title('Linear Regression')
-        # self.a.set_xlabel('X axis label')
-        # self.a.set_ylabel('Y label')
+class Graph(ttk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.create_widgets(master)
+        self.grid(sticky='w')
 
-        self.canvas = FigureCanvasTkAgg(self.f, master=master)
-        self.canvas.get_tk_widget().grid(row=1, column=0)
+    def exit(self):
+        self.master.master.destroy()
+
+    def create_widgets(self, master):
+        f = Figure(figsize=(7, 5), dpi=100)
+        a = f.add_subplot(111)
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2 * pi * t)
+
+        a.plot(t, s)
+
+        canvas = FigureCanvasTkAgg(f, master=master)
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas.get_tk_widget().grid(row=1, column=0)
